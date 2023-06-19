1c4e5bbead9ff8ccd4e6f9744665805b731985f0
Miquel Massot <miquel.massot@gmail.com>
2023-06-19 11:30:07 +0100

    Add GHA, pre-commit and bump version

--- setup.py
+++ setup.py
@@ -2,30 +2,32 @@ from __future__ import print_function
 import sys
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     INSTALL_REQUIRES = [l.strip() for l in f.readlines() if l]
 
 
 try:
     import numpy  # NOQA
 except ImportError:
-    print('numpy is required during installation')
+    print("numpy is required during installation")
     sys.exit(1)
 
 try:
     import scipy  # NOQA
 except ImportError:
-    print('scipy is required during installation')
+    print("scipy is required during installation")
     sys.exit(1)
 
 setup(
-    name='spherecluster',
-    version='0.1.7',
-    description='Clustering on the unit hypersphere in scikit-learn.',
-    author='Jason Laska',
-    author_email='jason@claralabs.com',
+    name="spherecluster",
+    version="0.2.0",
+    description="Clustering on the unit hypersphere in scikit-learn.",
+    author="Jason Laska",
+    author_email="jason@claralabs.com",
+    maintainer="Miquel Massot",
+    maintainer_email="miquel.massot@gmail.com",
     packages=find_packages(),
     install_requires=INSTALL_REQUIRES,
-    url='https://github.com/clara-labs/spherecluster',
-    license='MIT',
+    url="https://github.com/miquelmassot/spherecluster",
+    license="MIT",
 )
